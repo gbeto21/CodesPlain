6cf3acfea8641e9a2a518431222b03a5c5b33c91
Alberto <albertoglezb@hotmail.com>
2024-06-22 13:03:21 -0600

    A modfication to the Repositories Summary test was made.

--- src/components/repositories/RepositoriesSummary.test.js
+++ src/components/repositories/RepositoriesSummary.test.js
@@ -1,7 +1,7 @@
 import { screen, render } from "@testing-library/react";
 import RepositoriesSummary from "./RepositoriesSummary";
 
-test("Displays the primary language of the repository", () => {
+test("Displays information about the repository", () => {
   const repository = {
     language: "JavaScript",
     stargazers_count: 4,
@@ -10,6 +10,10 @@ test("Displays the primary language of the repository", () => {
   };
   render(<RepositoriesSummary repository={repository} />);
 
-  const language = screen.getByText("JavaScript");
-  expect(language).toBeInTheDocument();
+  for (let key in repository) {
+    const value = repository[key];
+    const element = screen.getByText(value);
+
+    expect(element).toBeInTheDocument();
+  }
 });
