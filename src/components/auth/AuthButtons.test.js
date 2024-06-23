d1b913185b13daef82fa0a54e72078be005ac7b5
Alberto <albertoglezb@hotmail.com>
2024-06-23 11:04:43 -0600

    First attempt to write the second set of test was implemented.

--- src/components/auth/AuthButtons.test.js
+++ src/components/auth/AuthButtons.test.js
@@ -45,18 +45,26 @@ describe("when user is not signed in", () => {
 
 describe("When user is signed in", () => {
   // createServer() ---> '/api/user' ---> {user: {id:3, email: 'asdf@a.com'}}
-  // createServer([
-  //   {
-  //     path: "/api/user",
-  //     res: () => {
-  //       return { user: { id: 1, email: "asdf@asdf.com" } };
-  //     },
-  //   },
-  // ]);
-  // test("sign in and sign up are not visible", async () => {
-  //   renderComponent();
-  // });
-  // test("sign out are is visible", async () => {
-  //   renderComponent();
-  // });
+  createServer([
+    {
+      path: "/api/user",
+      res: () => {
+        return { user: { id: 1, email: "asdf@asdf.com" } };
+      },
+    },
+  ]);
+  test("sign in and sign up are not visible", async () => {
+    await renderComponent();
+    const signInButton = screen.queryByRole("link", { name: /sign in/i });
+    const signUpButton = screen.queryByRole("link", { name: /sign up/i });
+
+    expect(signInButton).not.toBeInTheDocument();
+    expect(signUpButton).not.toBeInTheDocument();
+  });
+  test("sign out are is visible", async () => {
+    await renderComponent();
+    const signOutButton = screen.getByRole("link", { name: /sign out/i });
+    expect(signOutButton).toBeInTheDocument();
+    expect(signOutButton).toHaveAttribute("href", "/signout");
+  });
 });
