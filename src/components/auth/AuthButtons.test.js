e16f5c6e00807d97fdaf873d3589693d09ce0a0b
Alberto <albertoglezb@hotmail.com>
2024-06-23 10:35:50 -0600

    The 'describe' method is used on the AuthButtons file.

--- src/components/auth/AuthButtons.test.js
+++ src/components/auth/AuthButtons.test.js
@@ -3,28 +3,32 @@ import { MemoryRouter } from "react-router-dom";
 import { createServer } from "../../test/server";
 import AuthButtons from "./AuthButtons";
 
-// createServer() ---> '/api/user' ---> {user: null}
-createServer([
-  {
-    path: "/api/user",
-    res: () => {
-      return { user: null };
+describe("when user is not signed in", () => {
+  // createServer() ---> '/api/user' ---> {user: null}
+  createServer([
+    {
+      path: "/api/user",
+      res: () => {
+        return { user: null };
+      },
     },
-  },
-]);
-test("When user is not signed in, sign in and sign up are visible", async () => {});
+  ]);
+  test("When user is not signed in, sign in and sign up are visible", async () => {});
 
-test("When user is not signed in, sign out is not visible", async () => {});
+  test("When user is not signed in, sign out is not visible", async () => {});
+});
 
-// createServer() ---> '/api/user' ---> {user: {id:3, email: 'asdf@a.com'}}
-createServer([
-  {
-    path: "/api/user",
-    res: () => {
-      return { user: { id: 1, email: "asdf@asdf.com" } };
+describe("When user is signed in", () => {
+  // createServer() ---> '/api/user' ---> {user: {id:3, email: 'asdf@a.com'}}
+  createServer([
+    {
+      path: "/api/user",
+      res: () => {
+        return { user: { id: 1, email: "asdf@asdf.com" } };
+      },
     },
-  },
-]);
-test("When user is signed in, sign in and sign up are not visible", async () => {});
+  ]);
+  test("When user is signed in, sign in and sign up are not visible", async () => {});
 
-test("When user is signed in, sign out are is visible", async () => {});
+  test("When user is signed in, sign out are is visible", async () => {});
+});
