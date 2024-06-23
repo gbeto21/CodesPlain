7a32867ba86018e9fee8faaf8005af545c35dd7d
Alberto <albertoglezb@hotmail.com>
2024-06-23 10:28:58 -0600

    Added the create server functionality in the AuthButtons test.

--- src/components/auth/AuthButtons.test.js
+++ src/components/auth/AuthButtons.test.js
@@ -4,11 +4,27 @@ import { createServer } from "../../test/server";
 import AuthButtons from "./AuthButtons";
 
 // createServer() ---> '/api/user' ---> {user: null}
+createServer([
+  {
+    path: "/api/user",
+    res: () => {
+      return { user: null };
+    },
+  },
+]);
 test("When user is not signed in, sign in and sign up are visible", async () => {});
 
 test("When user is not signed in, sign out is not visible", async () => {});
 
 // createServer() ---> '/api/user' ---> {user: {id:3, email: 'asdf@a.com'}}
+createServer([
+  {
+    path: "/api/user",
+    res: () => {
+      return { user: { id: 1, email: "asdf@asdf.com" } };
+    },
+  },
+]);
 test("When user is signed in, sign in and sign up are not visible", async () => {});
 
 test("When user is signed in, sign out are is visible", async () => {});
